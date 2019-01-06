36a23abc150f1417eb3f26fc4c9ac3dbb978845f
georgemoralis <giorgosmrls@gmail.com>
2019-01-06 23:03:54 +0200

    Intial import from arcadeflex037b7

---
+++ emulator/src/arcadeflex/libc/cstring.java
@@ -0,0 +1,66 @@
+package arcadeflex.libc;
+
+import static arcadeflex.libc.ptr.*;
+import static common.ptr.*;
+
+/**
+ *
+ * @author shadow
+ */
+public class cstring {
+
+    /**
+     * MEMSET functions
+     */
+    public static void memset(char[] buf, int value, int size) {
+        for (int mem = 0; mem < size; mem++) {
+            buf[mem] = (char) value;
+        }
+    }
+
+    public static void memset(short[] dst, int value, int size) {
+        for (int mem = 0; mem < size; mem++) {
+            dst[mem] = (short) value;
+        }
+    }
+
+    public static void memset(int[] dst, int value, int size) {
+        for (int mem = 0; mem < size; mem++) {
+            dst[mem] = value;
+        }
+    }
+
+    public static void memset(UBytePtr ptr, int value, int length) {
+        for (int i = 0; i < length; i++) {
+            ptr.write(i, value);
+        }
+    }
+
+    public static void memset(UBytePtr ptr, int offset, int value, int length) {
+        for (int i = 0; i < length; i++) {
+            ptr.write(i + offset, value);
+        }
+    }
+
+    public static void memset(ShortPtr buf, int value, int size) {
+        for (int i = 0; i < size; i++) {
+            buf.write(i, (short) value);
+        }
+    }
+
+    /**
+     * MEMCPY functions
+     */
+    public static void memcpy(UBytePtr dst, int dstoffs, UBytePtr src, int size) {
+        for (int i = 0; i < Math.min(size, src.memory.length); i++) {
+            dst.write(i + dstoffs, src.read(i));
+        }
+    }
+    
+    /**
+     * STRCMP function
+     */
+    public static int strcmp(String str1, String str2) {
+        return str1.compareTo(str2);
+    }
+}
---
+++ emulator/src/old2/mame/mameH.java
@@ -0,0 +1,94 @@
+/*
+This file is part of Arcadeflex.
+
+Arcadeflex is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Arcadeflex is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Arcadeflex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * ported to 0.37b7
+ */
+package old2.mame;
+
+import static old.arcadeflex.libc_old.FILE;
+import static mame.commonH.GameSamples;
+import static mame.drawgfxH.GfxElement;
+import static mame.drawgfxH.rectangle;
+import static mame.driverH.GameDriver;
+import static mame.driverH.MachineDriver;
+import static old.mame.inptportH.InputPort;
+import static mame.osdependH.osd_bitmap;
+import static common.subArrays.*;
+
+public class mameH {
+
+    public static final int MAX_GFX_ELEMENTS = 32;
+    public static final int MAX_MEMORY_REGIONS = 32;
+
+    public static class RunningMachine {
+
+        public /*unsigned char * */ char[][] u8_memory_region = new char[MAX_MEMORY_REGIONS][];
+        public int memory_region_length[] = new int[MAX_MEMORY_REGIONS];
+        public int memory_region_type[] = new int[MAX_MEMORY_REGIONS];
+
+        public GfxElement gfx[] = new GfxElement[MAX_GFX_ELEMENTS];
+        public osd_bitmap scrbitmap;
+        public rectangle visible_area;
+        public /*UINT16 * */ char[] pens;
+        public /*UINT16 * */ char[] game_colortable;
+        public UShortArray remapped_colortable;
+        public GameDriver gamedrv;
+        public MachineDriver drv;
+        public int color_depth;
+        public int sample_rate;
+
+        public int obsolete;
+        public GameSamples samples;
+        public InputPort[] input_ports;
+        public InputPort[] input_ports_default;
+        public int orientation;
+        public GfxElement uifont;
+        public int uifontwidth, uifontheight;
+        public int uixmin, uiymin;
+        public int uiwidth, uiheight;
+        public int ui_orientation;
+        public rectangle absolute_visible_area;
+    }
+
+    public static class GameOptions {
+
+        public FILE record;
+        public FILE playback;
+        public FILE language_file;
+        public int mame_debug;
+        public int cheat;
+        public int gui_host;
+        public int samplerate;
+        public int use_samples;
+        public int use_emulated_ym3812;
+        public int color_depth;
+        public int vector_width;
+        public int vector_height;
+        public int norotate;
+        public int ror;
+        public int rol;
+        public int flipx;
+        public int flipy;
+        public int beam;
+        public int flicker;
+        public int translucency;
+        public int antialias;
+        public int use_artwork;
+
+    }
+
+}
