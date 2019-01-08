50abff54a6692d984fe4a46fa3d030051aebbe0f
Chuso <chusogar@gmail.com>
2019-01-08 11:57:38 +0100

    Displays version 0.37b11

--- emulator/src/old2/arcadeflex/settings.java
+++ emulator/src/old2/arcadeflex/settings.java
@@ -5,5 +5,5 @@ package old2.arcadeflex;
  * @author shadow
  */
 public class settings {
-    public static String version = "arcadeflex v0.37b7.1";
+    public static String version = "arcadeflex v0.37b11";
 }
