6deff40ce6f9f5904534f6f6d6d1f59a8cfcb6a6
georgemoralis <giorgosmrls@gmail.com>
2019-01-27 11:41:53 +0200

    More work on usrintrf porting

--- emulator/src/WIP/drivers/munchmo.java
+++ emulator/src/WIP/drivers/munchmo.java
@@ -3,15 +3,16 @@
  * using automatic conversion tool v0.01
  */
 package WIP.drivers;
+
 import static mame056.cpuintrfH.*;
 import static mame056.cpuexecH.*;
 import static arcadeflex.fucPtr.*;
-import static mame.commonH.*;
+import static mame056.commonH.*;
 import static mame.drawgfxH.*;
 import static mame.driverH.*;
 import static old.mame.inptport.*;
 import static old.mame.inptportH.*;
-import static old2.mame.memoryH.*;
+import static mame056.memoryH.*;
 import static mame.sndintrfH.*;
 import static vidhrdw.generic.*;
 import static sound.ay8910H.*;
@@ -57,69 +58,73 @@ public class munchmo {
         }
     };
 
-    static MemoryReadAddress readmem[]
+    static Memory_ReadAddress readmem[]
             = {
-                new MemoryReadAddress(0x0000, 0x3fff, MRA_ROM),
-                new MemoryReadAddress(0x8000, 0x83ff, MRA_RAM), /* working RAM */
-                new MemoryReadAddress(0xa000, 0xa3ff, MRA_RAM),
-                new MemoryReadAddress(0xa400, 0xa7ff, mnchmobl_sprite_xpos_r), /* mirrored */
-                new MemoryReadAddress(0xa800, 0xabff, MRA_RAM),
-                new MemoryReadAddress(0xac00, 0xafff, mnchmobl_sprite_tile_r), /* mirrored */
-                new MemoryReadAddress(0xb000, 0xb3ff, MRA_RAM),
-                new MemoryReadAddress(0xb400, 0xb7ff, mnchmobl_sprite_attr_r), /* mirrored */
-                new MemoryReadAddress(0xb800, 0xb8ff, MRA_RAM),
-                new MemoryReadAddress(0xb900, 0xb9ff, mnchmobl_videoram_r), /* mirrored */
-                new MemoryReadAddress(0xbe02, 0xbe02, input_port_3_r), /* DSW1 */
-                new MemoryReadAddress(0xbe03, 0xbe03, input_port_4_r), /* DSW2 */
-                new MemoryReadAddress(0xbf01, 0xbf01, input_port_0_r), /* coin, start */
-                new MemoryReadAddress(0xbf02, 0xbf02, input_port_1_r), /* P1 controls */
-                new MemoryReadAddress(0xbf03, 0xbf03, input_port_2_r), /* P2 controls */
-                new MemoryReadAddress(-1)
+                new Memory_ReadAddress(MEMPORT_MARKER, MEMPORT_DIRECTION_READ | MEMPORT_TYPE_MEM | MEMPORT_WIDTH_8),
+                new Memory_ReadAddress(0x0000, 0x3fff, MRA_ROM),
+                new Memory_ReadAddress(0x8000, 0x83ff, MRA_RAM), /* working RAM */
+                new Memory_ReadAddress(0xa000, 0xa3ff, MRA_RAM),
+                new Memory_ReadAddress(0xa400, 0xa7ff, mnchmobl_sprite_xpos_r), /* mirrored */
+                new Memory_ReadAddress(0xa800, 0xabff, MRA_RAM),
+                new Memory_ReadAddress(0xac00, 0xafff, mnchmobl_sprite_tile_r), /* mirrored */
+                new Memory_ReadAddress(0xb000, 0xb3ff, MRA_RAM),
+                new Memory_ReadAddress(0xb400, 0xb7ff, mnchmobl_sprite_attr_r), /* mirrored */
+                new Memory_ReadAddress(0xb800, 0xb8ff, MRA_RAM),
+                new Memory_ReadAddress(0xb900, 0xb9ff, mnchmobl_videoram_r), /* mirrored */
+                new Memory_ReadAddress(0xbe02, 0xbe02, input_port_3_r), /* DSW1 */
+                new Memory_ReadAddress(0xbe03, 0xbe03, input_port_4_r), /* DSW2 */
+                new Memory_ReadAddress(0xbf01, 0xbf01, input_port_0_r), /* coin, start */
+                new Memory_ReadAddress(0xbf02, 0xbf02, input_port_1_r), /* P1 controls */
+                new Memory_ReadAddress(0xbf03, 0xbf03, input_port_2_r), /* P2 controls */
+                new Memory_ReadAddress(MEMPORT_MARKER, 0)
             };
 
-    static MemoryWriteAddress writemem[]
+    static Memory_WriteAddress writemem[]
             = {
-                new MemoryWriteAddress(0x0000, 0x3fff, MWA_ROM),
-                new MemoryWriteAddress(0x8000, 0x83ff, MWA_RAM), /* working RAM */
-                new MemoryWriteAddress(0xa000, 0xa3ff, MWA_RAM, mnchmobl_sprite_xpos),
-                new MemoryWriteAddress(0xa400, 0xa7ff, mnchmobl_sprite_xpos_w),
-                new MemoryWriteAddress(0xa800, 0xabff, MWA_RAM, mnchmobl_sprite_tile),
-                new MemoryWriteAddress(0xac00, 0xafff, mnchmobl_sprite_tile_w),
-                new MemoryWriteAddress(0xb000, 0xb3ff, MWA_RAM, mnchmobl_sprite_attr),
-                new MemoryWriteAddress(0xb400, 0xb7ff, mnchmobl_sprite_attr_w),
-                new MemoryWriteAddress(0xb800, 0xb9ff, mnchmobl_videoram_w, videoram),
-                new MemoryWriteAddress(0xba00, 0xbbff, MWA_RAM),
-                new MemoryWriteAddress(0xbc00, 0xbc7f, MWA_RAM, mnchmobl_status_vram),
-                new MemoryWriteAddress(0xbe00, 0xbe00, mnchmobl_soundlatch_w),
-                new MemoryWriteAddress(0xbe01, 0xbe01, mnchmobl_palette_bank_w),
-                new MemoryWriteAddress(0xbe11, 0xbe11, MWA_RAM), /* ? */
-                new MemoryWriteAddress(0xbe21, 0xbe21, MWA_RAM), /* ? */
-                new MemoryWriteAddress(0xbe31, 0xbe31, MWA_RAM), /* ? */
-                new MemoryWriteAddress(0xbe41, 0xbe41, mnchmobl_flipscreen_w),
-                new MemoryWriteAddress(0xbe61, 0xbe61, mnchmobl_nmi_enable_w), /* ENI 1-10C */
-                new MemoryWriteAddress(0xbf00, 0xbf07, MWA_RAM, mnchmobl_vreg), /* MY0 1-8C */
-                new MemoryWriteAddress(-1)
+                new Memory_WriteAddress(MEMPORT_MARKER, MEMPORT_DIRECTION_WRITE | MEMPORT_TYPE_MEM | MEMPORT_WIDTH_8),
+                new Memory_WriteAddress(0x0000, 0x3fff, MWA_ROM),
+                new Memory_WriteAddress(0x8000, 0x83ff, MWA_RAM), /* working RAM */
+                new Memory_WriteAddress(0xa000, 0xa3ff, MWA_RAM, mnchmobl_sprite_xpos),
+                new Memory_WriteAddress(0xa400, 0xa7ff, mnchmobl_sprite_xpos_w),
+                new Memory_WriteAddress(0xa800, 0xabff, MWA_RAM, mnchmobl_sprite_tile),
+                new Memory_WriteAddress(0xac00, 0xafff, mnchmobl_sprite_tile_w),
+                new Memory_WriteAddress(0xb000, 0xb3ff, MWA_RAM, mnchmobl_sprite_attr),
+                new Memory_WriteAddress(0xb400, 0xb7ff, mnchmobl_sprite_attr_w),
+                new Memory_WriteAddress(0xb800, 0xb9ff, mnchmobl_videoram_w, videoram),
+                new Memory_WriteAddress(0xba00, 0xbbff, MWA_RAM),
+                new Memory_WriteAddress(0xbc00, 0xbc7f, MWA_RAM, mnchmobl_status_vram),
+                new Memory_WriteAddress(0xbe00, 0xbe00, mnchmobl_soundlatch_w),
+                new Memory_WriteAddress(0xbe01, 0xbe01, mnchmobl_palette_bank_w),
+                new Memory_WriteAddress(0xbe11, 0xbe11, MWA_RAM), /* ? */
+                new Memory_WriteAddress(0xbe21, 0xbe21, MWA_RAM), /* ? */
+                new Memory_WriteAddress(0xbe31, 0xbe31, MWA_RAM), /* ? */
+                new Memory_WriteAddress(0xbe41, 0xbe41, mnchmobl_flipscreen_w),
+                new Memory_WriteAddress(0xbe61, 0xbe61, mnchmobl_nmi_enable_w), /* ENI 1-10C */
+                new Memory_WriteAddress(0xbf00, 0xbf07, MWA_RAM, mnchmobl_vreg), /* MY0 1-8C */
+                new Memory_WriteAddress(MEMPORT_MARKER, 0)
             };
 
-    static MemoryReadAddress readmem_sound[]
+    static Memory_ReadAddress readmem_sound[]
             = {
-                new MemoryReadAddress(0x0000, 0x1fff, MRA_ROM),
-                new MemoryReadAddress(0x2000, 0x2000, soundlatch_r),
-                new MemoryReadAddress(0xe000, 0xe7ff, MRA_RAM),
-                new MemoryReadAddress(-1)
+                new Memory_ReadAddress(MEMPORT_MARKER, MEMPORT_DIRECTION_READ | MEMPORT_TYPE_MEM | MEMPORT_WIDTH_8),
+                new Memory_ReadAddress(0x0000, 0x1fff, MRA_ROM),
+                new Memory_ReadAddress(0x2000, 0x2000, soundlatch_r),
+                new Memory_ReadAddress(0xe000, 0xe7ff, MRA_RAM),
+                new Memory_ReadAddress(MEMPORT_MARKER, 0)
             };
-    static MemoryWriteAddress writemem_sound[]
+    static Memory_WriteAddress writemem_sound[]
             = {
-                new MemoryWriteAddress(0x0000, 0x1fff, MWA_ROM),
-                new MemoryWriteAddress(0x4000, 0x4000, AY8910_write_port_0_w),
-                new MemoryWriteAddress(0x5000, 0x5000, AY8910_control_port_0_w),
-                new MemoryWriteAddress(0x6000, 0x6000, AY8910_write_port_1_w),
-                new MemoryWriteAddress(0x7000, 0x7000, AY8910_control_port_1_w),
-                new MemoryWriteAddress(0x8000, 0x8000, MWA_NOP), /* ? */
-                new MemoryWriteAddress(0xa000, 0xa000, MWA_NOP), /* ? */
-                new MemoryWriteAddress(0xc000, 0xc000, MWA_NOP), /* ? */
-                new MemoryWriteAddress(0xe000, 0xe7ff, MWA_RAM),
-                new MemoryWriteAddress(-1)
+                new Memory_WriteAddress(MEMPORT_MARKER, MEMPORT_DIRECTION_WRITE | MEMPORT_TYPE_MEM | MEMPORT_WIDTH_8),
+                new Memory_WriteAddress(0x0000, 0x1fff, MWA_ROM),
+                new Memory_WriteAddress(0x4000, 0x4000, AY8910_write_port_0_w),
+                new Memory_WriteAddress(0x5000, 0x5000, AY8910_control_port_0_w),
+                new Memory_WriteAddress(0x6000, 0x6000, AY8910_write_port_1_w),
+                new Memory_WriteAddress(0x7000, 0x7000, AY8910_control_port_1_w),
+                new Memory_WriteAddress(0x8000, 0x8000, MWA_NOP), /* ? */
+                new Memory_WriteAddress(0xa000, 0xa000, MWA_NOP), /* ? */
+                new Memory_WriteAddress(0xc000, 0xc000, MWA_NOP), /* ? */
+                new Memory_WriteAddress(0xe000, 0xe7ff, MWA_RAM),
+                new Memory_WriteAddress(MEMPORT_MARKER, 0)
             };
 
     static AY8910interface ay8910_interface = new AY8910interface(
@@ -331,30 +336,30 @@ public class munchmo {
 
     static RomLoadPtr rom_joyfulr = new RomLoadPtr() {
         public void handler() {
-            ROM_REGION(0x10000, REGION_CPU1);/* 64k for CPUA */
+            ROM_REGION(0x10000, REGION_CPU1, 0);/* 64k for CPUA */
             ROM_LOAD("m1j.10e", 0x0000, 0x2000, 0x1fe86e25);
             ROM_LOAD("m2j.10d", 0x2000, 0x2000, 0xb144b9a6);
 
-            ROM_REGION(0x10000, REGION_CPU2);/* 64k for sound CPU */
+            ROM_REGION(0x10000, REGION_CPU2, 0);/* 64k for sound CPU */
             ROM_LOAD("mu.2j", 0x0000, 0x2000, 0x420adbd4);
 
-            ROM_REGION(0x2000, REGION_GFX1 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x2000, REGION_GFX1, ROMREGION_DISPOSE);
             ROM_LOAD("s1.10a", 0x0000, 0x1000, 0xc0bcc301);/* characters */
             ROM_LOAD("s2.10b", 0x1000, 0x1000, 0x96aa11ca);
 
-            ROM_REGION(0x2000, REGION_GFX2);
+            ROM_REGION(0x2000, REGION_GFX2, 0);
             ROM_LOAD("b1.2c", 0x0000, 0x1000, 0x8ce3a403);/* tile layout */
             ROM_LOAD("b2.2b", 0x1000, 0x1000, 0x0df28913);/* 4x8 tiles */
 
-            ROM_REGION(0x6000, REGION_GFX3 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x6000, REGION_GFX3, ROMREGION_DISPOSE);
             ROM_LOAD("f1j.1g", 0x0000, 0x2000, 0x93c3c17e);/* sprites */
             ROM_LOAD("f2j.3g", 0x2000, 0x2000, 0xb3fb5bd2);
             ROM_LOAD("f3j.5g", 0x4000, 0x2000, 0x772a7527);
 
-            ROM_REGION(0x2000, REGION_GFX4 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x2000, REGION_GFX4, ROMREGION_DISPOSE);
             ROM_LOAD("h", 0x0000, 0x2000, 0x332584de);/* monochrome sprites */
 
-            ROM_REGION(0x0100, REGION_PROMS);
+            ROM_REGION(0x0100, REGION_PROMS, 0);
             ROM_LOAD("a2001.clr", 0x0000, 0x0100, 0x1b16b907);/* color prom */
             ROM_END();
         }
@@ -362,30 +367,30 @@ public class munchmo {
 
     static RomLoadPtr rom_mnchmobl = new RomLoadPtr() {
         public void handler() {
-            ROM_REGION(0x10000, REGION_CPU1);/* 64k for CPUA */
+            ROM_REGION(0x10000, REGION_CPU1, 0);/* 64k for CPUA */
             ROM_LOAD("m1.10e", 0x0000, 0x2000, 0xa4bebc6a);
             ROM_LOAD("m2.10d", 0x2000, 0x2000, 0xf502d466);
 
-            ROM_REGION(0x10000, REGION_CPU2);/* 64k for sound CPU */
+            ROM_REGION(0x10000, REGION_CPU2, 0);/* 64k for sound CPU */
             ROM_LOAD("mu.2j", 0x0000, 0x2000, 0x420adbd4);
 
-            ROM_REGION(0x2000, REGION_GFX1 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x2000, REGION_GFX1, ROMREGION_DISPOSE);
             ROM_LOAD("s1.10a", 0x0000, 0x1000, 0xc0bcc301);/* characters */
             ROM_LOAD("s2.10b", 0x1000, 0x1000, 0x96aa11ca);
 
-            ROM_REGION(0x2000, REGION_GFX2);
+            ROM_REGION(0x2000, REGION_GFX2, 0);
             ROM_LOAD("b1.2c", 0x0000, 0x1000, 0x8ce3a403);/* tile layout */
             ROM_LOAD("b2.2b", 0x1000, 0x1000, 0x0df28913);/* 4x8 tiles */
 
-            ROM_REGION(0x6000, REGION_GFX3 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x6000, REGION_GFX3, ROMREGION_DISPOSE);
             ROM_LOAD("f1.1g", 0x0000, 0x2000, 0xb75411d4);/* sprites */
             ROM_LOAD("f2.3g", 0x2000, 0x2000, 0x539a43ba);
             ROM_LOAD("f3.5g", 0x4000, 0x2000, 0xec996706);
 
-            ROM_REGION(0x2000, REGION_GFX4 | REGIONFLAG_DISPOSE);
+            ROM_REGION(0x2000, REGION_GFX4, ROMREGION_DISPOSE);
             ROM_LOAD("h", 0x0000, 0x2000, 0x332584de);/* monochrome sprites */
 
-            ROM_REGION(0x0100, REGION_PROMS);
+            ROM_REGION(0x0100, REGION_PROMS, 0);
             ROM_LOAD("a2001.clr", 0x0000, 0x0100, 0x1b16b907);/* color prom */
             ROM_END();
         }
